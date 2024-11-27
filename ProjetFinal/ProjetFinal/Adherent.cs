1843daac0f550c1ddf7161143500bc7e435a3b4e
NoelLaurian <Noellaurian77@gmail.com>
2024-11-27 13:39:49 -0500

    lo

---
+++ ProjetFinal/ProjetFinal/Adherent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetFinal
+{
+    internal class Adherent
+    {
+        string id_Adherent;
+        string nom;
+        string prenom;
+        string adresse;
+        string date_naissance;
+        int age;
+        int id_Admin;
+
+
+        public Adherent()
+        {
+            this.id_Adherent = "";
+            this.nom = "";
+            this.prenom = "";
+            this.adresse = "";
+            this.date_naissance = "";
+            this.age = 0;
+            this.id_Admin = 0;
+
+        }
+        public Adherent(string id_Adherent, string nom, string prenom, string adresse, string date_naissance, int age, int id_Admin)
+        {
+            this.id_Adherent = id_Adherent;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.adresse = adresse;
+            this.date_naissance = date_naissance;
+            this.age = age;
+            this.id_Admin = id_Admin;
+        }
+
+        public string Id_Adherent { get => id_Adherent; set => id_Adherent = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Adresse { get => adresse; set => adresse = value; }
+        public string Date_naissance { get => date_naissance; set => date_naissance = value; }
+        public int Age { get => age; set => age = value; }
+        public int Id_Admin { get => id_Admin; set => id_Admin = value; }
+
+
+
+        public override string ToString()
+        {
+            return nom;
+        }
+    }
+}
